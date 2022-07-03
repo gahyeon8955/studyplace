0c8d6581895e0ec7fb723596cca57d6836322991
gahyeon8955 <rkgus8955@likelion.org>
2022-07-03 13:42:19 +0900

    mainapp

--- studyplace/settings.py
+++ studyplace/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'place.apps.PlaceConfig',
+    'mainapp.apps.MainappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- studyplace/urls.py
+++ studyplace/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import place.views
+import mainapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', place.views.home, name='home'),
+    path('', mainapp.views.home, name='home'),
 ]
