1bfc8acd7c818ad5452700e0433a4a017e89d5ff
gahyeon8955 <rkgus8955@likelion.org>
2022-07-13 15:24:03 +0900

    placelist design

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'mainapp/home.html')
+
+def placelist(request):
+    return render(request, 'mainapp/placelist.html')
